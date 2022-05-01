aed37c75731e4960e9ca08e7813d5895c89f4bd0
Nguyễn Kim Ngàn <84651190+nganpt054@users.noreply.github.com>
2022-05-01 07:46:53 +0700

    update-5

--- SV18T1021193.Web/Controllers/ProductController.cs
+++ SV18T1021193.Web/Controllers/ProductController.cs
@@ -6,12 +6,97 @@ using System.Web.Mvc;
 
 namespace SV18T1021193.Web.Controllers
 {
+    /// <summary>
+    /// 
+    /// </summary>
     [Authorize]
+    [RoutePrefix("product")]
     public class ProductController : Controller
     {
-        // GET: Product
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <returns></returns>
         public ActionResult Index()
         {
+            return View();
+        }
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <returns></returns>
+        public ActionResult Create()
+        {
+            return View();
+        }
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="productID"></param>
+        /// <returns></returns>
+        [Route("edit/{productID}")]
+        public ActionResult Edit(int productID)
+        {
+            return View();
+        }
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="productID"></param>
+        /// <returns></returns>
+        [Route("delete/{productID}")]
+        public ActionResult Delete(int productID)
+        {
+            return View();
+        }
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="method"></param>
+        /// <param name="productID"></param>
+        /// <param name="photoID"></param>
+        /// <returns></returns>
+        [Route("photo/{method}/{productID}/{photoID?}")]
+        public ActionResult Photo(string method, int productID, int? photoID)
+        {
+            switch (method)
+            {
+                case "add":
+                    ViewBag.Title = "Bổ sung ảnh";
+                    break;
+                case "edit":
+                    ViewBag.Title = "Thay đổi ảnh";
+                    break;
+                case "delete":
+                    return RedirectToAction("Edit", new { productID = productID });
+                default:
+                    return RedirectToAction("Index");
+            }
+            return View();
+        }
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="method"></param>
+        /// <param name="productID"></param>
+        /// <param name="attributeID"></param>
+        /// <returns></returns>
+        [Route("attribute/{method}/{productID}/{attributeID?}")]
+        public ActionResult Attribute(string method, int productID, int? attributeID)
+        {
+            switch (method)
+            {
+                case "add":
+                    ViewBag.Title = "Bổ sung thuộc tính";
+                    break;
+                case "edit":
+                    ViewBag.Title = "Thay đổi thuộc tính";
+                    break;
+                case "delete":
+                    return RedirectToAction("Edit", new { productID = productID });
+                default:
+                    return RedirectToAction("Index");
+            }
             return View();
         }
     }
